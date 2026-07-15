e51688ee862f87e8de2bb19ab532a22d592fa288
Bary. Roshini <roshinibari@gmail.com>
2026-07-15 14:30:27 +0530

    Add files via upload

---
+++ frontend.py
@@ -0,0 +1,32 @@
+import gradio as gr
+import requests
+
+backend_url = "https://sql-query-generator-backend-1fnk.onrender.com"
+
+def generate_sql(question):
+
+    response = requests.post(
+        backend_url,
+        json={"question": question}
+    )
+
+    if response.status_code == 200:
+        return response.json()["sql"]
+    else:
+        return "Error connecting to backend."
+
+demo = gr.Interface(
+    fn=generate_sql,
+    inputs=gr.Textbox(
+        label="Enter English Description",
+        placeholder="Example: Show all students from CSE with marks above 80",
+        lines=4
+    ),
+    outputs=gr.Textbox(
+        label="Generated SQL Query",
+        lines=8
+    ),
+    title="AI SQL Query Generator"
+)
+
+demo.launch()
